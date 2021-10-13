10aaf01ba35b1c5a5ed120941702da86911087a8
Scott Donnan <scott.donnan@me.com>
2021-10-12 21:58:02 -0500

    commit tree 819ef75581830fb8f485e7dbacf36624d0ad88f5

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves dwarves
+  dwarves.each.with_index do |val, index|
+    puts "#{index+1}. #{val}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+def summon_captain_planet captain
+  captain.map do |element|
+    "#{element.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls calls
+  calls.any? do |call|
+    call.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese cheeses
+  cheeses.find do |cheese|
+    cheese_types = ["cheddar", "gouda", "camembert"]
+    if cheese_types.include?(cheese)
+      cheese
+    else
+      nil
+    end
+  end
 end
